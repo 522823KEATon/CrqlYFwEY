7d322de77a3bcdde34841cb2a19383f520a80fc9
522823KEATon <woks.07jibs@icloud.com>
2025-09-19 20:13:42 +0900

    Build new 交互式图表生成器 feature

---
+++ interactive_chart_generator_0919_1913_gja.js
@@ -0,0 +1,97 @@
+// 代码生成时间: 2025-09-19 19:13:41
+ * interactive_chart_generator.js
+ * This program uses Hapi.js to create an interactive chart generator.
+ * It allows users to specify chart type, data, and options to generate an interactive chart.
+ */
+
+const Hapi = require('@hapi/hapi');
+const Joi = require('joi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const Handlebars = require('handlebars');
+const Fs = require('fs');
+const Path = require('path');
+
+// Create a server with a host and port
+const server = Hapi.server({
+  host: 'localhost',
+  port: 3000,
+});
+
+// Register Inert and Vision plugins for serving static files and templating
+async function startServer() {
+  await server.register(Inert);
+  await server.register(Vision);
+  await server.views({
+    engines: { html: Handlebars },
+    relativeTo: __dirname,
+    path: 'templates',
+  });
+
+  // Define the route for generating an interactive chart
+  server.route({
+    method: 'GET',
+    path: '/interactive-chart',
+    options: {
+      // Validate query parameters
+      validate: {
+        query: Joi.object().keys({
+          type: Joi.string().required().description('The type of chart'),
+          data: Joi.array().required().description('The data for the chart'),
+          options: Joi.object().description('Options for the chart'),
+        }),
+      },
+      handler: async (request, h) => {
+        // Extract query parameters
+        const { type, data, options } = request.query;
+
+        // Generate the chart (simplified for demonstration purposes)
+        const chart = generateChart(type, data, options);
+
+        // Render the chart template with chart data
+        return h.view('chart_template', { chart });
+      },
+    });
+
+  // Start the server
+  await server.start();
+  console.log('Server running at:', server.info.uri);
+}
+
+async function generateChart(type, data, options) {
+  // Simplified chart generation logic
+  // In a real-world scenario, this would integrate with a charting library
+  return {
+    type,
+    data,
+    options,
+  };
+}
+
+// Serve static files for the front-end
+server.route({
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: './public',
+    },
+  },
+});
+
+// Start the server
+startServer();
+
+// Define the Handlebars template for the chart
+Handlebars.registerPartial('chartTemplate', Fs.readFileSync(Path.join(__dirname, 'templates/chart_template.hbs'), 'utf8'));
+
+/*
+ * Chart Template using Handlebars
+ * This template will be used to render the interactive chart.
+ * It should be saved as chart_template.hbs in the templates directory.
+ * {{> chartTemplate}}
+ *
+ * Example template structure:
+ * <div id="chart" data-type="{{chart.type}}" data-data={{chart.data}} data-options={{chart.options}}></div>
+ * <script src="static/js/chart.js"></script>
+ */
